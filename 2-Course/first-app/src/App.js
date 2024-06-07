d5ce391cb08a25b5e3d37f09cfef57c32d9712c2
Utkarsh Pratap Singh <113666652+Thakurutkarsh07@users.noreply.github.com>
2024-06-08 00:19:46 +0530

    updated

---
+++ 2-Course/first-app/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const NAME = "Utkarsh Thakur";
+  return (
+    <>
+    <div>
+      <h1 style={{
+        color:"red",
+      }}>
+        This is a new project and created by {NAME} </h1>
+    </div>
+    <div>
+      <p>this is a paragraph</p>
+    </div>
+    </>
+  );
+}
+
+export default App;
